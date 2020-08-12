70cf31511cc69c561904fb966c8ee33d3379156d
haytham43 <haytham.halimeh@gmail.com>
2020-08-13 01:46:40 +0300

    first working decoder

---
+++ src/decoder.js
@@ -0,0 +1,251 @@
+const debug = require('debug')('llrpjs:Decoder');
+const MgBuf = require('./managed-buffer');
+
+function isEmpty(obj) {
+    return Object.keys(obj).length === 0 && obj.constructor === Object
+}
+
+function groupBy (array, key) {
+    return array.reduce((obj, item) => {
+        return {
+            ...obj,
+            [item[key]]: item,
+        };
+    }, {});
+};
+
+function Decoder(llrpdef, options) {
+    if (!(this instanceof Decoder)) return new Decoder(...arguments);
+
+    let defaultOpt = {
+        format: {
+            "u64": "iso-8601",
+            "u96": "hex"
+        }
+    };
+    this.opt = {...defaultOpt, ...options};
+
+    this.llrpdef = llrpdef;
+
+    this.msgDefByTypeNum = groupBy(llrpdef.messageDefinitions, "typeNum");
+    this.paramDefByTypeNum = groupBy(llrpdef.parameterDefinitions, "typeNum");
+    this.paramDefByName = groupBy(llrpdef.parameterDefinitions, "name");
+    this.choiceDefByName = groupBy(llrpdef.choiceDefinitions, "name");
+    this.enumDefByName = groupBy(llrpdef.enumerationDefinitions, "name");
+
+    this.mBuf = null;
+}
+/**
+ * 1) read the message header and extract type
+ * 2) get the definition from type
+ * 3) iterate through the fields, parameters, choices, etcs
+ * 4) for fields and enumurations, just read them from buffer and return their values
+ * 5) in parameters and choices, get their definitions and iterate through their content similarly
+ */
+
+Decoder.prototype.message = function (buf) {
+    this.buf = buf;
+    let msg = {};
+    this.mBuf = new MgBuf(buf);
+
+    let typeNum = this.mBuf.get_u16();
+    let version = (typeNum >>> 10) & 0x03;
+    typeNum &= 0x3ff;
+
+    if (version != 1)
+        throw new Error(`unsupported version`);
+
+    let length = this.mBuf.get_u32();
+    if (length < 10)
+        throw new Error(`message length too small`);
+
+    if (length > buf.length)
+        throw new Error(`buffer length too small`);
+
+    let def = this.msgDefByTypeNum[typeNum];
+    if (!def)
+        throw new Error(`unknown type`);
+
+    msg["MessageType"] = def["name"];
+    let messageID = this.mBuf.get_u32();
+    msg["MessageID"] = messageID;
+
+    let body;
+    if (typeNum == 1023) {
+        // custom message
+        // check if we have a definition for it, if not use the generic one
+    } else {
+        body = this.definition.call(this, def.body);
+    }
+    msg["MessageBody"] = body;
+    return msg;
+}
+
+Decoder.prototype.definition = function (def) {
+    let body = {}
+    for (let i in def) {
+        if (!this.mBuf.idx.bitsLeft)
+            break;
+        let defRef = def[i];
+        let node = defRef.node;
+        let decoder = this._getPropertyDecoder.call(this, node);
+        let result = {};
+        if (["parameter", "choice"].includes(node)) {
+            let resArray = [];
+            let repeat = defRef.repeat;
+            while (true) {
+                if (!this.mBuf.idx.bitsLeft)
+                    break;
+
+                let resItem = decoder.call(this, defRef);
+
+                if (isEmpty(resItem))
+                    break;
+                resArray.push(resItem);
+
+                if (repeat.endsWith("1"))   // max one occurence allowed
+                    break;
+            }
+            if (resArray.length > 1) {
+                let key = Object.keys(resArray[0])[0];
+                result = {...result, ...{[key]: resArray.map(x=>x[key])}};
+            } else if (resArray.length == 1) {
+                result = {...result, ...resArray[0]};
+            }
+        } else {
+            result = decoder.call(this, defRef);
+        }
+
+        body = {...body, ...result};
+    }
+    return body;
+}
+
+Decoder.prototype.field = function (def) {
+    let fieldOps = this._getFieldOps.call(this, def.type);
+    let result = {};
+    let fieldValue = fieldOps();
+    if (def.enumeration) {
+        // process enum
+        let enumDef = this.enumDefByName[def.enumeration];
+        if (!enumDef)
+            throw new Error(`unknown enum ${def.enumeration}`);
+        let enumDefByValue = groupBy(enumDef.entry, "value");
+        if (!enumDefByValue[fieldValue])
+            throw new Error(`unknown ${def.enumeration} enum entry ${fieldValue}`);
+        fieldValue = enumDefByValue[fieldValue].name;
+    }
+    result[def.name] = fieldValue;
+    return result;
+}
+
+Decoder.prototype.parameter = function (defRef) {
+    let repeat = defRef.repeat;
+    let startBit = this.mBuf.idx.bit;
+    let startByte = this.mBuf.idx.byte;
+    let type = this.mBuf.get_u8();
+    let length = 0;
+    if (type & 0x80) {
+        // TV ... contains all fields no worries!
+        type &= 0x7f;
+    } else {
+        // TLV
+        type = ((type & 0x03) << 8) + this.mBuf.get_u8();
+        length = this.mBuf.get_u16();
+        if (length < 4) {
+            throw new Error(`TLV parameter length is too short ${length}`);
+        }
+    }
+    let def = this.paramDefByName[defRef.type]
+
+    if (def.typeNum != type) {
+        this.mBuf.idx.bit = startBit;
+        this.mBuf.idx.byte = startByte;
+        return {};
+    }
+
+    return { 
+        [def.name]: this.definition.call(this, def.body)
+    };
+}
+
+Decoder.prototype.choice = function (defRef) {
+    // replace choice defRef with parameter defRef
+    let startBit = this.mBuf.idx.bit;
+    let startByte = this.mBuf.idx.byte;
+    let type = this.mBuf.get_u8();
+    let length = 0;
+    if (type & 0x80) {
+        // TV ... contains all fields no worries!
+        type &= 0x7f;
+    } else {
+        // TLV
+        type = ((type & 0x03) << 8) + this.mBuf.get_u8();
+    }
+    this.mBuf.idx.bit = startBit;
+    this.mBuf.idx.byte = startByte;
+    let paramDef = this.paramDefByTypeNum[type];
+    let choiceDef = this.choiceDefByName[defRef.type];
+    // check if it's really one of our parameters in the buffer
+    if (!choiceDef.body.filter(x=>x.type == paramDef.name).length) {
+        console.log(paramDef, choiceDef);
+        return {};
+    }
+
+    return this.parameter.call(this, {
+        node: "parameter",
+        type: paramDef.name,
+        repeat: defRef.repeat
+    });
+}
+
+Decoder.prototype.reserved = function(def) {
+    this.mBuf.get_reserved(Number(def.bitCount));
+    return {};
+}
+
+Decoder.prototype._getPropertyDecoder = function (node) {
+    return {
+        "field": this.field.bind(this),
+        "parameter": this.parameter.bind(this),
+        "choice": this.choice.bind(this),
+        "reserved": this.reserved.bind(this)
+    }[node] || (()=>{throw new Error(`no decoder for node ${node}`)});
+}
+
+Decoder.prototype._getFieldOps = function (type) {
+    return {
+        "u1": this.mBuf.get_u1.bind(this.mBuf),
+        "u2": this.mBuf.get_u2.bind(this.mBuf),
+  
+        "u1v": this.mBuf.get_u1v.bind(this.mBuf),
+  
+        "u8": this.mBuf.get_u8.bind(this.mBuf),
+        "s8": this.mBuf.get_s8.bind(this.mBuf),
+        "u8v": this.mBuf.get_u8v.bind(this.mBuf),
+        "s8v": this.mBuf.get_s8v.bind(this.mBuf),
+  
+        "utf8v": this.mBuf.get_utf8v.bind(this.mBuf),
+  
+        "u16": this.mBuf.get_u16.bind(this.mBuf),
+        "s16": this.mBuf.get_s16.bind(this.mBuf),
+        "u16v": this.mBuf.get_u16v.bind(this.mBuf),
+        "s16v": this.mBuf.get_s16v.bind(this.mBuf),
+  
+        "u32": this.mBuf.get_u32.bind(this.mBuf),
+        "s32": this.mBuf.get_s32.bind(this.mBuf),
+        "u32v": this.mBuf.get_u32v.bind(this.mBuf),
+        "s32v": this.mBuf.get_s32v.bind(this.mBuf),
+  
+        "u64": this.mBuf.get_u64.bind(this.mBuf),
+        "s64": this.mBuf.get_s64.bind(this.mBuf),
+        "u64v": this.mBuf.get_u64v.bind(this.mBuf),
+        "s64v": this.mBuf.get_s64v.bind(this.mBuf),
+  
+        "u96": this.mBuf.get_u96.bind(this.mBuf),
+  
+        "bytesToEnd": this.mBuf.get_bytesToEnd.bind(this.mBuf)
+    }[type];
+}
+
+module.exports = Decoder;
