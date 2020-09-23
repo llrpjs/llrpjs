507222ca2f7bbf5e56f2d807256eab8f3ac1fecb
haytham43 <haytham.halimeh@gmail.com>
2020-09-24 00:54:08 +0300

    added type parsers

---
+++ src/parsers.js
@@ -0,0 +1,40 @@
+
+module.exports = {
+    u1v: (value, format)=>{
+        if (format == "Hex")
+            return value.length?
+                value.split('').map(x=>parseInt(x, 16).toString(2).padStart(4, 0)).join('').split('').map(x=>parseInt(x,2))
+                : [];
+        else
+            return value;
+    },
+    utf8v: (value, format)=>{
+        return value.toString('utf8');
+    },
+    u8v: (value, format)=>{
+        if (format == "Hex") {
+            return value.length?
+                value.split('').reduce((acc, x, i)=>(i%2==0) && (i != 0)?`${acc},${x}`:`${acc}${x}`, '').split(',')
+                    .map(x=>parseInt(x.padEnd(2, 0), 16)) : [];
+        } else
+            return value;
+    },
+    u16v: (value, format)=>{
+        if (format == "Hex") {
+            return value.length?
+                value.split('').reduce((acc, x, i)=>(i%4==0) && (i != 0)?`${acc},${x}`:`${acc}${x}`, '').split(',')
+                    .map(x=>parseInt(x.padEnd(4, 0), 16)) : [];
+        } else
+            return value;
+    },
+    u64: (value, format)=>{
+        if (format == "Datetime")
+            // return microseconds
+            return BigInt(value) * 1000n;
+        else
+            return value;
+    },
+    u96: (value, format)=>{return module.exports.u8v(value, format)},
+    bytesToEnd: (value, format)=>{return module.exports.u8v(value, format)},
+    no_parser: (value, format)=>value
+};
