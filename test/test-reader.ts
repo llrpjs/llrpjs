490b070a661974a3195d68786ea88f2f59e6a559
haytham43 <haytham.halimeh@gmail.com>
2021-04-06 02:03:10 +0300

    update

--- test/test-reader.ts
+++ test/test-reader.ts
@@ -1,22 +1,10 @@
-// Server
-import { LLRPDef } from "../src/def";
-import { LLRPMessageFactory } from "../src/LLRPFactory";
-import { LLRPReader } from "../src";
-import { TypeRegistry } from "../src/type-registry";
-
-//import net from "net";
-import { LLRPMessage } from "../src/LLRPMessage";
-
+import { LLRPMessage, LLRPClient, LLRPCoreDataTypes, LLRPCore } from "../src/";
 import net from "net";
-
-TypeRegistry.getInstance().enrollCoreDefinitions(LLRPDef).build();
-
-const LLRPCoreMessages = LLRPMessageFactory(LLRPDef);
 
 
 const server = net.createServer(function (socket) {
     const timer = setInterval(() => {
-        socket.write(new LLRPCoreMessages.KEEPALIVE().encode().getBuffer());
+        //        socket.write(new LLRPCoreMessages.KEEPALIVE().encode().getBuffer());
     }, 1500);
 
     socket.on("close", () => {
@@ -29,90 +17,89 @@ const server = net.createServer(function (socket) {
     socket.on("data", data => {
         const msg = new LLRPMessage(data).decode();
         if (msg.getName() === "KEEPALIVE_ACK") return;
-        setTimeout(() => {
-            const rsp = new LLRPCoreMessages.ADD_ROSPEC_RESPONSE({
-                data: {
-                    LLRPStatus: {
-                        StatusCode: "M_Success",
-                        ErrorDescription: "ROSpec added successfully"
-                    }
+        const rsp = new LLRPCore.ADD_ROSPEC_RESPONSE({
+            id: msg.getMessageId(),
+            data: {
+                LLRPStatus: {
+                    StatusCode: "M_Success",
+                    ErrorDescription: "ROSpec added successfully"
                 }
-            });
-            socket.write(rsp.encode().getBuffer());
-        }, 200);
+            }
+        });
+        socket.write(rsp.encode().getBuffer());
     });
 });
 
-server.listen(1337, '127.0.0.1');
+server.listen(5084, '127.0.0.1');
 
 
 // Client
-
-const reader = new LLRPReader({
+const reader = new LLRPClient({
     host: "localhost",
-    port: 1337
+    port: 5084
 });
 
-const msg = new LLRPCoreMessages.ADD_ROSPEC({
-    data: {
-        ROSpec: {
-            ROSpecID: 12345,
-            Priority: 2,
-            CurrentState: "Disabled",
-            ROBoundarySpec: {
-                ROSpecStartTrigger: {
-                    ROSpecStartTriggerType: "Null"
+const addROSpecData: LLRPCoreDataTypes['ADD_ROSPEC'] = {
+    ROSpec: {
+        ROSpecID: 12345,
+        Priority: 2,
+        CurrentState: "Disabled",
+        ROBoundarySpec: {
+            ROSpecStartTrigger: {
+                ROSpecStartTriggerType: "Null"
+            },
+            ROSpecStopTrigger: {
+                ROSpecStopTriggerType: "Null",
+                DurationTriggerValue: 0
+            }
+        },
+        AISpec: [
+            {
+                AntennaIDs: [1, 2, 3, 4],
+                AISpecStopTrigger: {
+                    AISpecStopTriggerType: "Duration",
+                    DurationTrigger: 5000
                 },
-                ROSpecStopTrigger: {
-                    ROSpecStopTriggerType: "Null",
-                    DurationTriggerValue: 0
+                InventoryParameterSpec: {
+                    InventoryParameterSpecID: 1,
+                    ProtocolID: "EPCGlobalClass1Gen2"
                 }
             },
-            AISpec: [
-                {
-                    AntennaIDs: [1, 2, 3, 4],
-                    AISpecStopTrigger: {
-                        AISpecStopTriggerType: "Duration",
-                        DurationTrigger: 5000
-                    },
-                    InventoryParameterSpec: {
-                        InventoryParameterSpecID: 1,
-                        ProtocolID: "EPCGlobalClass1Gen2"
-                    }
+            {
+                AntennaIDs: [3, 4],
+                AISpecStopTrigger: {
+                    AISpecStopTriggerType: "Null",
+                    DurationTrigger: 0
                 },
-                {
-                    AntennaIDs: [3, 4],
-                    AISpecStopTrigger: {
-                        AISpecStopTriggerType: "Null",
-                        DurationTrigger: 0
-                    },
-                    InventoryParameterSpec: {
-                        InventoryParameterSpecID: 2,
-                        ProtocolID: "EPCGlobalClass1Gen2"
-                    }
+                InventoryParameterSpec: {
+                    InventoryParameterSpecID: 2,
+                    ProtocolID: "EPCGlobalClass1Gen2"
                 }
-            ],
-            RFSurveySpec: {
-                AntennaID: 1,
-                StartFrequency: 980000,
-                EndFrequency: 990000,
-                RFSurveySpecStopTrigger: {
-                    StopTriggerType: "N_Iterations_Through_Frequency_Range",
-                    DurationPeriod: 0,
-                    N: 10
-                }
+            }
+        ],
+        RFSurveySpec: {
+            AntennaID: 1,
+            StartFrequency: 980000,
+            EndFrequency: 990000,
+            RFSurveySpecStopTrigger: {
+                StopTriggerType: "N_Iterations_Through_Frequency_Range",
+                DurationPeriod: 0,
+                N: 10
             }
         }
     }
-});
+};
 
 (async () => {
     try {
         await reader.connect();
         for (let i = 0; i < 100; i++) {
-            console.log("send message");
+            const msg = new LLRPCore.ADD_ROSPEC({
+                data: addROSpecData
+            });
+            console.log(`send ${msg.getName()} ${msg.getMessageId()}`);
             const rsp = await reader.transact(msg);
-            console.log(rsp.toLLRPData());
+            console.log(`recv ${rsp.getName()} ${rsp.getMessageId()}`);
         }
     } catch (e) {
         console.error(e);
@@ -124,18 +111,30 @@ const msg = new LLRPCoreMessages.ADD_ROSPEC({
 
 reader.on("message", msg => {
     const data = msg.toLLRPData();
-    if (data.type === "KEEPALIVE") {
-        reader.send(new LLRPCoreMessages.KEEPALIVE_ACK()).catch(console.error);
-    }
 });
+
+reader.on("READER_EVENT_NOTIFICATION", msg => {
+    let gpiEvent = msg.getReaderEventNotificationData()
+        .getGPIEvent();
+    if (1 === gpiEvent.getGPIPortNumber()) {
+        //        doSomethingSerious();
+    } else {
+        //        wellMeh();
+    }
+})
 
 reader.on("KEEPALIVE", msg => {
     console.log(`KEEPALIVE listener`);
+    reader.send(new LLRPCore.KEEPALIVE_ACK({
+        id: msg.getMessageId(),
+        data: {}
+    })).catch(console.error);
 });
 
-reader.on("ADD_ROSPEC_RESPONSE", msg => {
-    console.log(`ADD_ROSPEC_RESPONSE listener`);
-});
+// reader.on("ADD_ROSPEC_RESPONSE", msg => {
+//     console.log(`ADD_ROSPEC_RESPONSE listener`);
+//     console.log(msg.getLLRPStatus().getStatusCode());
+// });
 
 reader.on("error", err => {
     console.error(err);
